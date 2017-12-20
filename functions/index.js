e2dbbb941820309b99ff1f2dddcedc8ca6b7ccd0
Nguyễn Minh Tiến <minhminhtien1992@gmail.com>
2017-12-20 10:08:28 +0700

    test functions

--- functions/index.js
+++ functions/index.js
@@ -7,32 +7,35 @@ admin.initializeApp(functions.config().firebase);
 
 //count unread message 
 exports.unreadMessage = functions.https.onRequest((request, response) => {
-  var db = admin.database();
-  // var ref = db.ref("dinosaurs");
-  // ref.orderByChild("dimensions/height").on("child_added",function(snapshot){
-  //   console.log(snapshot.key + " ___was __" + snapshot.val().height);
-  // })
+  var height = request.query.height;
+  var weight = request.query.weight;    
 
+  var db = admin.database();
   var ref = db.ref("dinosaurs");
   ref.orderByChild("dimensions/height").on("child_added", function(snapshot) {
-    console.log(snapshot.key + " was " + JSON.stringify(snapshot.val()) + " meters tall");
+    console.log(snapshot.key + " was " + snapshot.val().dimensions.height);
   });
-  //query order by value
-  var scoresRef = db.ref("scores");
-  scoresRef.orderByValue().on("value", function(snapshot) {
-    snapshot.forEach(function(data) {
-      console.log("The " + data.key + " dinosaur's score is " + data.val());
-    });
+
+  ref.orderByChild("height").equalTo(height).on("child_added",function(snapshot){
+    console.log(snapshot.key+ " and this is equal function: " + JSON.stringify(snapshot.val()));
   });
-  //query limit
-  var ref = db.ref("dinosaurs");
-  ref.orderByChild("weight").limitToLast(2).on("child_added", function(snapshot) {
-    console.log(snapshot.key);
-  });
-  //equal
-  var ref = db.ref("dinosaurs");
-  ref.orderByChild("height").equalTo(25).on("child_added", function(snapshot) {
-    console.log(snapshot.key);
-  });
+
+  // //query order by value
+  // var scoresRef = db.ref("scores");
+  // scoresRef.orderByValue().on("value", function(snapshot) {
+  //   snapshot.forEach(function(data) {
+  //     console.log("The " + data.key + " dinosaur's score is " + data.val());
+  //   });
+  // });
+  // //query limit
+  // var ref = db.ref("dinosaurs");
+  // ref.orderByChild("weight").limitToLast(2).on("child_added", function(snapshot) {
+  //   console.log(snapshot.key);
+  // });
+  // //equal
+  // var ref = db.ref("dinosaurs");
+  // ref.orderByChild("height").equalTo(25).on("child_added", function(snapshot) {
+  //   console.log(snapshot.key);
+  // });
   response.send("Just save successful !");
 });
